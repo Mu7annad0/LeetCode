08ccf56b4303ead4dbd89cf66d43dfd48b1bbeea
Mu7annadd <mu7annad.0@gmail.com>
2023-01-06 11:59:03 +0200

    python

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashma = {}
 
--- 53_maxSubArray.py
+++ 53_maxSubArray.py
@@ -1,7 +1,7 @@
+
 # 53. Maximum Subarray
 # Given an integer array nums, find the subarray with the largest sum, and return its sum.
 # https://leetcode.com/problems/maximum-subarray/description/
-
 
 
 def maxSubArray(self, nums):
