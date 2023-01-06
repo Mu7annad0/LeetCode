f01cea89c3cf032b01ac21cdb67065be25f9cfec
Muhannad Ashraf <121406590+Mu7annad0@users.noreply.github.com>
2023-01-06 09:41:13 +0200

    Create 53.py

---
+++ 53.py
@@ -0,0 +1,17 @@
+# 53. Maximum Subarray
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+# https://leetcode.com/problems/maximum-subarray/description/
+
+
+
+def maxSubArray(self, nums):
+        max_sub = nums[0]
+        cur_sub = 0
+
+        for n in nums:
+            if cur_sub < 0:
+                cur_sub = 0
+            cur_sub += n
+            max_sub = max(max_sub, cur_sub)
+        return max_sub
+      
