247af86cc86042570944bacde4cdea0d64cf5640
Muhannad Ashraf <121406590+Mu7annad0@users.noreply.github.com>
2023-01-06 09:39:06 +0200

    Update 1491.py

--- 1491.py
+++ 1491.py
@@ -5,6 +5,7 @@ from typing import List
 # You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
 # Return the average salary of employees excluding the minimum and maximum salary.
 # Answers within 10-5 of the actual answer will be accepted.
+# https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/description/
 
 def average(self, salary: List[int]) -> float:
     salary.remove(min(salary))
