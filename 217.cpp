61a10b9f61f1bb13e886252f891aab0a797efc2e
Muhannad Ashraf <121406590+Mu7annad0@users.noreply.github.com>
2023-01-06 09:34:31 +0200

    Create 217.cpp

---
+++ 217.cpp
@@ -0,0 +1,12 @@
+// 217. Contains Duplicate
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        bool flag = false;
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return flag;
+    }
