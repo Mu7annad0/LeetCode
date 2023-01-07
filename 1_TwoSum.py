5bc555d0e322d3d573f502606997e3b8d469864a
Mu7annadd <mu7annad.0@gmail.com>
2023-01-07 08:29:27 +0200

    python

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -1,3 +1,11 @@
+# 1. Two Sum 
+# Given an array of integers nums and an integer target, 
+# return indices of the two numbers such that they add up to target.
+# https://leetcode.com/problems/two-sum/description/
+
+
+from typing import List
+
 def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashma = {}
 
